378fc79878027d4b2f48811d193c8795fc0490ee
Rostyslav Lysenko <educ@i.ua>
2022-09-29 00:25:20 +0300

    feat: added server config struct

--- cfg/config.go
+++ cfg/config.go
@@ -3,8 +3,20 @@
 // that will be retrieved from  .env or ./cfg/config.yml
 package cfg
 
+import "time"
+
+// Server configuration description
+type ServerConfig struct {
+	Host         string
+	Port         string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	IdleTimeout  time.Duration
+}
+
 // Options will keep all needful configs
-type Options struct{}
+type Options struct {
+}
 
 // NewOptions will create instance of Options
 // that will be used im main package
