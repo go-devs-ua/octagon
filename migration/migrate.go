cf5fdf964fa9987ad287c3a46f53d095959baa63
Serhii <sergeyjuly@gmail.com>
2022-09-30 14:02:35 +0300

    fix: change in migration from store to simple db connection

--- migration/migrate.go
+++ migration/migrate.go
@@ -1,33 +1,32 @@
 package migration
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
-
-	"github.com/go-devs-ua/octagon/app/repository/pg"
 )
 
 // Execute migrations.
 // Use "migrate-up" argument after run of program. It will create new table
 // Or use "migrate-down" - that will remove table
 // Example: go run main.go migrate-up
-func Migrate(repo *pg.Repo) error {
+func Migrate(db *sql.DB) error {
 	for _, arg := range os.Args {
 		if arg == "migrate-up" {
 			fmt.Println("Starting up")
-			execMigrationQuery(repo, "./migration/migrationsUp.sql")
+			execMigrationQuery(db, "./migration/migrationsUp.sql")
 		}
 
 		if arg == "migrate-down" {
 			fmt.Println("Starting down")
-			execMigrationQuery(repo, "./migration/migrationsDown.sql")
+			execMigrationQuery(db, "./migration/migrationsDown.sql")
 		}
 	}
 
 	return nil
 }
 
-func execMigrationQuery(repo *pg.Repo, fileName string) error {
+func execMigrationQuery(db *sql.DB, fileName string) error {
 	byteQuery, err := os.ReadFile(fileName)
 	fmt.Println(string(byteQuery))
 	if err != nil {
@@ -38,7 +37,7 @@ func execMigrationQuery(repo *pg.Repo, fileName string) error {
 		return err
 	}
 
-	if _, err := repo.DB.Exec(string(byteQuery)); err != nil {
+	if _, err := db.Exec(string(byteQuery)); err != nil {
 		return fmt.Errorf("cannot exec query: %w", err)
 	}
 	return nil
