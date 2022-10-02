c64d0527ba4a71abe0fd92d393355ae4b7265683
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-02 09:19:09 +0300

    refactor: move handler to user

--- app/transport/http/user.go
+++ app/transport/http/user.go
@@ -6,6 +6,20 @@ import (
 	"github.com/go-devs-ua/octagon/app/entities"
 )
 
+// UserHandler is User HTTP handler
+// which consist of embedded UserUsecase interface
+type UserHandler struct {
+	usecase UserUsecase
+}
+
+// NewUserHandler will return a new instance
+// of UserHandler struct accepting UserUsecase interface
+func NewUserHandler(usecase UserUsecase) UserHandler {
+	return UserHandler{
+		usecase: usecase,
+	}
+}
+
 func (uh UserHandler) Map() []HandlerMap {
 	return []HandlerMap{
 		{EndPoint: "/user", Handler: uh.CreateUser(), Method: http.MethodGet},
