fe94af3a1716eeec8948819fb0b03bacf2b42809
lifeho5e <lifehouseburning@gmail.com>
2022-10-03 13:54:04 +0300

    feat: adding func to insert user into DB

--- app/repository/pg/user.go
+++ app/repository/pg/user.go
@@ -5,6 +5,7 @@ package pg
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/go-devs-ua/octagon/app/entities"
 	"github.com/go-devs-ua/octagon/cfg"
@@ -22,6 +23,14 @@ func NewRepo(opt cfg.Options) *Repo {
 // without even knowing it that allow us to decouple our layers
 // and will make our app flexible and maintainable.
 func (r *Repo) Add(usr entities.User) error {
-	// INSERT INTO "users" ...
+	sqlStatement := `
+INSERT INTO users (first_name, last_name, email, password)
+VALUES ($1, $2, $3, $4)
+RETURNING id`
+	err := r.QueryRow(sqlStatement, usr.FirstName, usr.LastName, usr.Email, usr.Password)
+	if err != nil {
+		log.Println(err)
+	}
+
 	return nil
 }
