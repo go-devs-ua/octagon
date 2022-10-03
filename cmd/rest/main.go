9aef229f45f68d1266a7e4d1c3954ed47d66ee62
Serhii Dzhulai <37537963+Brigant@users.noreply.github.com>
2022-10-03 14:14:16 +0300

    feat: add connection to DB, add new package for migrations (#4)
    * feat: add connection to DB, add new package for migrations
    
    * fix: Non-blocker
    
    * fix: Non-blocker
    
    * fix: raname i -> arg
    
    * fix: Call connection in main
    
    * feat: add .gitignore
    
    * fix: change in migration from store to simple db connection
    
    * fix: minor fix
    
    * fix: minor fix
    
    * fix: relocate log.Print to Run func
    
    * fix: change Optiongs struct

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -3,12 +3,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
 	"github.com/go-devs-ua/octagon/app/repository/pg"
 	rest "github.com/go-devs-ua/octagon/app/transport/http"
 	"github.com/go-devs-ua/octagon/app/usecase"
 	"github.com/go-devs-ua/octagon/cfg"
+	"github.com/go-devs-ua/octagon/migration"
 )
 
 func main() {
@@ -21,7 +24,18 @@ func main() {
 func Run() error {
 	// TODO: Handle errors, migration, configs ...
 	opt := cfg.NewOptions()
-	repo := pg.NewRepo(opt)
+
+	db, err := connectDB(opt)
+	if err != nil {
+		return err
+	}
+
+	repo := pg.NewRepo(db)
+
+	log.Println("Connected to DB")
+
+	migration.Migrate(db)
+
 	logic := usecase.NewUser(repo)
 	mux := rest.NewRouter()
 	srv := rest.NewServer(logic, mux)
@@ -29,4 +43,20 @@ func Run() error {
 		return err
 	}
 	return nil
+}
+
+func connectDB(opt cfg.Options) (*sql.DB, error) {
+	str := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
+		opt.DB.Host, opt.DB.Port, opt.DB.Username, opt.DB.Password, opt.DB.DBName)
+
+	db, err := sql.Open("postgres", str)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("ping to database failed: %w", err)
+	}
+
+	return db, nil
 }
