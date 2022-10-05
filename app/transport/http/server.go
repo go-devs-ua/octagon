d8b0636c04d6c6a9178afe18f684114e02e5fa3c
Vlada Shakhvorostova <vdshakh@gmail.com>
2022-10-05 14:58:46 +0300

    fix: comment

--- app/transport/http/server.go
+++ app/transport/http/server.go
@@ -8,19 +8,21 @@ import (
 	"time"
 )
 
+const timeoutMsg = "Connection timeout"
+
 // Server is simple server
 type Server struct{ *http.Server }
 
 // NewServer will initialize the server
 func NewServer(opt cfg.Options, handler UserHandler) *Server {
-	r := new(mux.Router)
+	router := new(mux.Router)
 
-	attachUserEndpoints(r, handler)
+	attachUserEndpoints(router, handler)
 
 	return &Server{
 		Server: &http.Server{
 			Addr:         opt.Server.Host,
-			Handler:      http.TimeoutHandler(r, 3*time.Second, "Connection timeout"),
+			Handler:      http.TimeoutHandler(router, 3*time.Second, timeoutMsg),
 			ReadTimeout:  2 * time.Second,
 			WriteTimeout: 5 * time.Second,
 		},
