95cec2430e84bde70f1ce1f43e130a36a8af2d57
Yurii Yakovenko <tk.retry@gmail.com>
2022-10-05 19:45:35 +0300

    fix: change response func and add const.go

--- app/transport/http/resp.go
+++ app/transport/http/resp.go
@@ -2,24 +2,28 @@ package http
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
 
-func GenerateJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
+//Usually, you need to make a rollback mechanism. If there is an error, roll back the changes
+//from the database. But we will not do that now. Therefore, we will not change the status code
+// after an error occurs
+func WriteJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
+	errRespond := map[string]interface{}{
+		"error": InternalServerError,
+	}
+
 	if data == nil {
-		log.Println("Data can not be empty")
-		return
+		log.Println("Data can not be empty") // we need to log the error when we select the logger
+		WriteJSONResponse(w, statusCode, errRespond)
 	}
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
-		fmt.Printf("could not encode json: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		errorMessage := "Internal Server Error"
-		w.Write([]byte(errorMessage))
+		log.Printf("could not encode json: %v", err) //we need to log the error when we select the logge
+		WriteJSONResponse(w, statusCode, errRespond)
 	}
 }
