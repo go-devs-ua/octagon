4c525e75676040e640487cfa984fb405cf91eac9
Yurii Yakovenko <tk.retry@gmail.com>
2022-10-06 10:05:27 +0300

    fix: added a return to exit the function on error

--- app/transport/http/resp.go
+++ app/transport/http/resp.go
@@ -9,9 +9,9 @@ import (
 //Usually, you need to make a rollback mechanism. If there is an error, roll back the changes
 //from the database. But we will not do that now. Therefore, we will not change the status code
 // after an error occurs
-func WriteJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
+func WriteJSONResponse(rw http.ResponseWriter, statusCode int, data interface{}) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(statusCode)
 
 	errRespond := map[string]interface{}{
 		"error": InternalServerError,
@@ -19,11 +19,12 @@ func WriteJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) 
 
 	if data == nil {
 		log.Println("Data can not be empty") // we need to log the error when we select the logger
-		WriteJSONResponse(w, statusCode, errRespond)
+		WriteJSONResponse(rw, statusCode, errRespond)
+		return
 	}
 
-	if err := json.NewEncoder(w).Encode(data); err != nil {
+	if err := json.NewEncoder(rw).Encode(data); err != nil {
 		log.Printf("could not encode json: %v", err) //we need to log the error when we select the logge
-		WriteJSONResponse(w, statusCode, errRespond)
+		WriteJSONResponse(rw, statusCode, errRespond)
 	}
 }
