05bdfa31258e76564498a3f6d9b738cd1363f681
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-07 20:59:01 +0300

    refactor: rm redundant ifaces

--- app/transport/http/contracts.go
+++ app/transport/http/contracts.go
@@ -1,26 +1,10 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/go-devs-ua/octagon/app/entities"
 )
 
 // UserUsecase represents User use-case layer
 type UserUsecase interface {
 	Signup(entities.User) error
-}
-
-// Router represents router (multiplexer)
-type Router interface {
-	mapRoutes(UserHandler)
-	http.Handler
-}
-
-// Logger represents logger
-type Logger interface {
-	Errorf(string, ...any)
-	Debugf(string, ...any)
-	Infof(string, ...any)
-	Warnf(string, ...any)
 }
