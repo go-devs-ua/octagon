9dddc0df9de1db993cc67075b19ea40939a918ba
Eug Blk <106852417+delveper@users.noreply.github.com>
2022-10-07 20:30:22 +0300

    Feat/create user add response func (#18)
    * feat: add func for response
    
    * fix: change response func and add const.go
    
    * fix: added a return to exit the function on error
    
    * fix: delete return in case where data == nil
    
    * fix: recursion removed
    
    * feat: add const msgs
    
    * feat: add const msgs
    
    * feat: add response func and error handling
    
    * refactor: auto
    
    * fix: capitalize msgs
    
    * fix: err msg
    
    * refactor: comments
    
    * fix: a minor
    
    Co-authored-by: Yurii Yakovenko <tk.retry@gmail.com>

---
+++ app/transport/http/const.go
@@ -0,0 +1,8 @@
+package http
+
+const (
+	MsgUserCreated      = "User successfully created"
+	MsgEmailConflict    = "Email is already taken"
+	MsgInternalSeverErr = "Internal server error"
+	MsgBadRequest       = "Bad request"
+)
