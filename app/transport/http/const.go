9715d4203279d5c080c14d8156ddfd7cfc5f05c9
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-07 15:10:15 +0300

    feat: add const msgs

---
+++ app/transport/http/const.go
@@ -0,0 +1,7 @@
+package http
+
+const (
+	MsgUserCreated      = "user successfully created"
+	MsgEmailConflict    = "email is already taken"
+	MsgInternalSeverErr = "internal server error"
+)
--- app/transport/http/resp.go
+++ app/transport/http/resp.go
@@ -6,9 +6,18 @@ import (
 	"net/http"
 )
 
-//Usually, you need to make a rollback mechanism. If there is an error, roll back the changes
-//from the database. But we will not do that now. Therefore, we will not change the status code
-// after an error occurs
+// Response will wrap message that will be sent in JSON format
+type Response struct {
+	Message string `json:"message"`
+}
+
+// TODO: Usually, you need to make a rollback mechanism.
+//  If there is an error, roll back the changes
+//  from the database. But we will not do that now.
+//  Therefore, we will not change the status code
+//  after an error occurs
+
+// WriteJSONResponse writes JSON response
 func WriteJSONResponse(rw http.ResponseWriter, statusCode int, data interface{}) {
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(statusCode)
@@ -19,6 +28,6 @@ func WriteJSONResponse(rw http.ResponseWriter, statusCode int, data interface{})
 	}
 
 	if err := json.NewEncoder(rw).Encode(data); err != nil {
-		log.Printf("could not encode json: %v", err) //we need to log the error when we select the logge
+		log.Printf("could not encode json: %v", err) // we need to log the error when we select the logge
 	}
 }
