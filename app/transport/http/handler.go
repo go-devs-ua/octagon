8a1f9425d3b172336b1593865335f2a5a2169623
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-07 21:03:56 +0300

    refactor: switch iface to logger pgk

--- app/transport/http/handler.go
+++ app/transport/http/handler.go
@@ -1,15 +1,19 @@
 package http
 
+import (
+	"github.com/go-devs-ua/octagon/app/logger"
+)
+
 // UserHandler is User HTTP handler
 // which consist of embedded UserUsecase interface
 type UserHandler struct {
 	usecase UserUsecase
-	logger  Logger
+	logger  *logger.Logger
 }
 
 // NewUserHandler will return a new instance
 // of UserHandler struct accepting UserUsecase interface
-func NewUserHandler(usecase UserUsecase, logger Logger) UserHandler {
+func NewUserHandler(usecase UserUsecase, logger *logger.Logger) UserHandler {
 	return UserHandler{
 		usecase: usecase,
 		logger:  logger,
