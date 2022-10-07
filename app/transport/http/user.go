0620dd8a756332072001f3b8953ea93eb84ec5ee
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-07 15:23:07 +0300

    feat: add response func and error handling

--- app/transport/http/user.go
+++ app/transport/http/user.go
@@ -2,27 +2,46 @@ package http
 
 import (
 	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/go-devs-ua/octagon/app/entities"
+	"github.com/lib/pq"
 )
 
 // CreateUser will handle user creation
 func (uh UserHandler) CreateUser() http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		user := entities.User{}
 
 		if err := json.NewDecoder(req.Body).Decode(&user); err != nil {
-			return //someone have to work with errors
+			WriteJSONResponse(w, http.StatusBadRequest, Response{MsgBadRequest})
+			return
 		}
-		defer req.Body.Close()
+
+		defer func() {
+			if err := req.Body.Close(); err != nil {
+				log.Println(err)
+			}
+		}()
 
 		if err := user.Validate(); err != nil {
-			return //someone have to work with errors
+			WriteJSONResponse(w, http.StatusBadRequest, Response{err.Error()})
+			return
 		}
 
 		if err := uh.usecase.Signup(user); err != nil {
-			return //someone have to work with errors
+			// It is driving me nuts, please help. We need to decouple from pq package
+			if err, ok := errors.Unwrap(err).(*pq.Error); ok && err.Code.Name() == "unique_violation" {
+				WriteJSONResponse(w, http.StatusConflict, Response{MsgEmailConflict})
+				return
+			}
+
+			WriteJSONResponse(w, http.StatusInternalServerError, Response{MsgInternalSeverErr})
+			return
 		}
+		// we could pass user here
+		WriteJSONResponse(w, http.StatusCreated, Response{MsgUserCreated})
 	})
 }
