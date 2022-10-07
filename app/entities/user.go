0f58509b52e44384294079bff2f79e99bee7debb
Lexando <106885277+Lexando@users.noreply.github.com>
2022-10-07 22:27:15 +0300

    feature validator (#10)
    * feature validator
    
    * Fixed
    
    * Fixed 2
    
    * Fixed 3
    
    * Fixed 4
    
    * Fixed 5
    
    Co-authored-by: Ruslan Li <98584319+Rusli4k@users.noreply.github.com>

--- app/entities/user.go
+++ app/entities/user.go
@@ -1,12 +1,11 @@
-// Package entities will consist of key entities  of our project
-// and represents core/domain layer of our app -  correct me if I am wrong
-// and would not have any dependencies on other layers.
-// ent package also contains validations and custom errors.
 package entities
 
-// User is key entity in our project
-// Entities like User are the least likely to change
-// when something external changes.
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
 type User struct {
 	ID        string `json:"id"`
 	Email     string `json:"email"`
@@ -16,9 +15,58 @@ type User struct {
 	CreatedAt string `json:"created_at"`
 }
 
-// TODO: sentinel errors
-
-// Validate will validate User's signup data
 func (u User) Validate() error {
+	if err := checkName(u.FirstName); err != nil {
+		return err
+	}
+
+	if u.LastName != "" {
+		if err := checkName(u.LastName); err != nil {
+			return err
+		}
+	}
+
+	if err := checkMail(u.Email); err != nil {
+		return err
+	}
+
+	if err := checkPass(u.Password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkName(name string) error {
+	if valid, _ := regexp.MatchString(`^[\p{L}&\s-\\'’.]{2,256}$`, name); !valid {
+		return errors.New("invalid name")
+	}
+
+	return nil
+}
+
+func checkMail(email string) error {
+	const (
+		maxLocalEmailLen  int = 64
+		maxDomainEmailLen int = 255
+	)
+	// Checking the total length (allowed no more than 64+1+255=320 symbols)
+	emailLen := len(email)
+	if emailLen > maxLocalEmailLen+1+maxDomainEmailLen {
+		return errors.New("email contents too many symbols: " + strconv.Itoa(emailLen))
+	}
+	// Checking for some email issues by regular expression
+	if valid, _ := regexp.MatchString("(?i)"+`^([^\.@(),:;<>@[\\\]]?[\da-z!#$%&'*+,\-/=?^_\x60{|}~]+.?)+@([^.-][\da-z-]*[^.-]\.?)+([a-z]{2,6})$`, email); !valid {
+		return errors.New("email does not match with regexp \"(?i)\"+`^([^\\.@(),:;<>@[\\\\\\]]?[\\da-z!#$%&'*+,\\-/=?^_\\x60{|}~]+.?)+@([^.-][\\da-z-]*[^.-]\\.?)+([a-z]{2,6})$`")
+	}
+
+	return nil
+}
+
+func checkPass(password string) error {
+	if valid, _ := regexp.MatchString(`^[[:graph:]]{2,256}$`, password); !valid {
+		return errors.New("invalid password")
+	}
+
 	return nil
 }
