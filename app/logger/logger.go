65f958328f62ddc3329a16b362531cd15328ee42
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-07 20:54:45 +0300

    refactor: auto

--- app/logger/logger.go
+++ app/logger/logger.go
@@ -6,38 +6,36 @@ import (
 	"go.uber.org/zap"
 )
 
-type Wrapper struct{ log *zap.SugaredLogger }
+type Logger struct{ log *zap.SugaredLogger }
 
-func NewLogger() *Wrapper {
+func New() *Logger {
 	// TODO: Extend config and make adjustments
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatalf("error creating new logger: %v", err)
 	}
 
-	return &Wrapper{logger.Sugar()}
+	return &Logger{logger.Sugar()}
 }
 
-func (w *Wrapper) Flush() {
+func (w *Logger) Flush() {
 	if err := w.log.Sync(); err != nil {
 		w.log.Error(err)
 	}
 }
 
-// Methods above will be implemented on transport layer according to http.Logger
-
-func (w *Wrapper) Errorf(format string, val ...any) {
+func (w *Logger) Errorf(format string, val ...any) {
 	w.log.Errorf(format, val)
 }
 
-func (w *Wrapper) Debugf(format string, val ...any) {
+func (w *Logger) Debugf(format string, val ...any) {
 	w.log.Debugf(format, val)
 }
 
-func (w *Wrapper) Infof(format string, val ...any) {
+func (w *Logger) Infof(format string, val ...any) {
 	w.log.Infof(format, val)
 }
 
-func (w *Wrapper) Warnf(format string, val ...any) {
+func (w *Logger) Warnf(format string, val ...any) {
 	w.log.Warnf(format, val)
 }
