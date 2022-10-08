84123199b2b6056a604e4693e19cdb6867b5f627
Yevhen Bilyk <yevhen@bilyk.dev>
2022-10-08 17:43:53 +0300

    fix: constant bug

--- app/transport/http/const.go
+++ app/transport/http/const.go
@@ -1,6 +1,7 @@
 package http
 
 const (
+	MsgUserCreated      = "User created successfully"
 	MsgEmailConflict    = "Email is already taken"
 	MsgInternalSeverErr = "Internal server error"
 	MsgBadRequest       = "Bad request"
