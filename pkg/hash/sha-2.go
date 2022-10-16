0e7c8bf66232416a4b22a5b0cca3a1a91ca976b8
YuriiYakovenko <106862480+YuriiYakovenko@users.noreply.github.com>
2022-10-16 15:01:20 +0300

    Update sha-2.go

--- pkg/hash/sha-2.go
+++ pkg/hash/sha-2.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func Hash(password string) string {
-	h := sha256.Sum256([]byte(password))
+func Hash(someString string) string {
+	h := sha256.Sum256([]byte(someString))
 	return fmt.Sprintf("%x", h)
 }
