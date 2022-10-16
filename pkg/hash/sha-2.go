0431c42ccca8a080357cb86eecb6b710f1400388
Lexando <106885277+Lexando@users.noreply.github.com>
2022-10-16 22:25:12 +0300

    feat: pswd hashing added (#22)
    * feat: pswd hashing added
    
    * fix: hasing removed to a seperate func
    
    * changed the implementation of the function and its use
    
    * Update .env
    
    * fix: pointer and comment delete
    
    * fix: rename hashing function
    
    * Update sha-2.go
    
    * refactor: changed func name, add description
    
    Co-authored-by: Yurii Yakovenko <tk.retry@gmail.com>
    Co-authored-by: YuriiYakovenko <106862480+YuriiYakovenko@users.noreply.github.com>

---
+++ pkg/hash/sha-2.go
@@ -0,0 +1,12 @@
+package hash
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+//SHA256 function returns the checksum of the string using SHA256 hash algorithms.
+func SHA256(someString string) string {
+	h := sha256.Sum256([]byte(someString))
+	return fmt.Sprintf("%x", h)
+}
