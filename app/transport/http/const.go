06b3125aa1e3dd1905d1681acbe4df6b8c6f5353
Rostyslav Lysenko <educ@i.ua>
2022-10-18 19:05:04 +0300

    fix: extended error messages

--- app/transport/http/const.go
+++ app/transport/http/const.go
@@ -3,6 +3,6 @@ package http
 const (
 	MsgUserCreated      = "User created successfully"
 	MsgEmailConflict    = "Email is already taken"
-	MsgInternalSeverErr = "Astrologers proclaim the week of server errors: "
+	MsgInternalSeverErr = "Astrologers proclaim the week of server errors..: "
 	MsgBadRequest       = "Provided JSON file is invalid: "
 )
