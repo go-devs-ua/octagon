c925ef79d8208472e99fee81ba223a6b65e198c4
Vlada Shakhvorostova <vdshakh@gmail.com>
2022-10-19 16:38:12 +0300

    fix: length of the email field

--- migration/002_create_user_table.sql
+++ migration/002_create_user_table.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE "user" ALTER COLUMN email TYPE VARCHAR(10);
+ALTER TABLE "user" ALTER COLUMN email TYPE VARCHAR(320);
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
