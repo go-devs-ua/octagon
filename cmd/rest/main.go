3ecfb1be77d50ab8b1fb039c3a93e593dedd5804
lifehou5e <106427843+lifehou5e@users.noreply.github.com>
2022-10-19 14:09:11 +0300

    feat: refactoring migrations, adding new (#23)
    * feat:refactoring migrations, adding new
    
    * reseting .env
    
    * fix:adding flags instead of os.Args
    
    * fix:using cfg.DB in ConnectDB() + quickfix
    
    * fix:removing redundant structs'
    
    * fix:adding check for flag bad input
    
    * fix:changing logger for fmt.Print
    
    * fix:minor fix
    
    * fix:fixing some minor issues from meeting 17.10
    
    * fix:minor
    
    * adding comment

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -3,8 +3,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 
 	"github.com/go-devs-ua/octagon/app/repository/pg"
@@ -12,7 +10,6 @@ import (
 	"github.com/go-devs-ua/octagon/app/usecase"
 	"github.com/go-devs-ua/octagon/cfg"
 	"github.com/go-devs-ua/octagon/lgr"
-	"github.com/go-devs-ua/octagon/migration"
 )
 
 func main() {
@@ -26,13 +23,13 @@ func Run() error {
 	logger := lgr.New()
 	defer logger.Flush()
 
-	opt, err := cfg.GetConfig()
+	config, err := cfg.GetConfig()
 	if err != nil {
 		logger.Errorf("Failed to get config from .env: %+v\n", err)
 		return err
 	}
 
-	db, err := connectDB(opt)
+	db, err := pg.ConnectDB(config.DB)
 	if err != nil {
 		logger.Errorf("%+v\n", err)
 		return err
@@ -41,36 +38,15 @@ func Run() error {
 	repo := pg.NewRepo(db)
 	logger.Infof("Connection to database successfully created\n")
 
-	if err := migration.Migrate(db, logger); err != nil {
-		logger.Errorf("Failed making migrations: %+v\n", err)
-		return err
-	}
-
 	handlers := rest.Handlers{
 		UserHandler: rest.NewUserHandler(usecase.NewUser(repo), logger),
 	}
 
-	srv := rest.NewServer(opt, handlers, logger)
+	srv := rest.NewServer(config, handlers, logger)
 	if err := srv.Run(); err != nil {
 		logger.Errorf("Failed loading server: %+v\n", err)
 		return err
 	}
 
 	return nil
-}
-
-func connectDB(opt cfg.Options) (*sql.DB, error) {
-	str := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
-		opt.DB.Host, opt.DB.Port, opt.DB.Username, opt.DB.Password, opt.DB.DBName)
-
-	db, err := sql.Open("postgres", str)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open database connection: %w", err)
-	}
-
-	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("ping to database failed: %w", err)
-	}
-
-	return db, nil
 }
