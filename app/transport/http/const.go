31e7b4cfa9a070219198bd87fd27a73f6506c67a
Rostyslav Lysenko <educ@i.ua>
2022-10-19 18:52:33 +0300

    fix: changed const description

--- app/transport/http/const.go
+++ app/transport/http/const.go
@@ -1,8 +1,8 @@
 package http
 
 const (
-	MsgUserCreated   = "User created successfully with ID: "
+	MsgUserCreated   = "User created successfully with ID"
 	MsgEmailConflict = "Email is already taken"
-	MsgValidationErr = "Validation error: "
-	MsgBadRequest    = "Provided JSON file is invalid: "
+	MsgValidationErr = "Validation error"
+	MsgBadRequest    = "Provided JSON file is invalid"
 )
