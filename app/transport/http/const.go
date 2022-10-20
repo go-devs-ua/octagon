e529b5d7190ed27774871b79b21355b10c78bcef
Rostyslav Lysenko <educ@i.ua>
2022-10-20 15:30:38 +0300

    fix: deleted unused constans

--- app/transport/http/const.go
+++ app/transport/http/const.go
@@ -1,9 +1,6 @@
 package http
 
 const (
-	MsgUserCreated   = "User created successfully with the following ID"
-	MsgEmailConflict = "Email is already taken"
-	MsgValidationErr = "Validation error"
-	MsgBadRequest    = "Bad Request"
-	MsgServerErr     = "Internal server error"
+	BadRequestMsg = "Bad Request"
+	ServerErrMsg  = "Internal server error"
 )
