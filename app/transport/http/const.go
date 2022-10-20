54b6702d7b7f013ed070e09a6a8a9acdb5b2d067
Rostyslav Lysenko <educ@i.ua>
2022-10-20 13:53:12 +0300

    fix: changed MsgBadRequest descr

--- app/transport/http/const.go
+++ app/transport/http/const.go
@@ -4,6 +4,6 @@ const (
 	MsgUserCreated   = "User created successfully with the following ID"
 	MsgEmailConflict = "Email is already taken"
 	MsgValidationErr = "Validation error"
-	MsgBadRequest    = "Server was unable to process the request due to incorrect syntax, invalid request message framing, or deceptive request routing."
+	MsgBadRequest    = "Bad Request"
 	MsgServerErr     = "Internal server error"
 )
