8d22c720d9e85131d8350014807b446c6c72d9eb
Rostyslav Lysenko <educ@i.ua>
2022-10-25 14:51:40 +0300

    refactor: idMask moved to entities

--- app/transport/rest/const.go
+++ app/transport/rest/const.go
@@ -10,5 +10,4 @@ const (
 	handlerTimeoutSeconds = 30
 	readTimeoutSeconds    = 2
 	writeTimeoutSeconds   = 5
-	idMask                = "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$"
 )
