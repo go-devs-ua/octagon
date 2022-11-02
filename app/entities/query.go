59c3362c8903fa8a7c29162fa6814a75119b6415
Eug Blk <yevhen@bilyk.dev>
2022-11-02 08:13:59 +0200

    refactor: add empty arg err msgs

--- app/entities/query.go
+++ app/entities/query.go
@@ -16,22 +16,30 @@ type QueryParams struct {
 
 // Validate checks if QueryParameter fields are valid.
 func (qp QueryParams) Validate() error {
+	if qp.Limit == "" {
+		return fmt.Errorf("limit argument has to be set")
+	}
+
 	if _, err := strconv.Atoi(qp.Limit); err != nil {
 		return fmt.Errorf("limit argument has to be a number")
+	}
+
+	if qp.Offset == "" {
+		return fmt.Errorf("offset argument has to be set")
 	}
 
 	if _, err := strconv.Atoi(qp.Offset); err != nil {
 		return fmt.Errorf("offset argument has to be a number")
 	}
 
-	allowedSortArgs := []string{"first_name", "last_name", "created_at", ","}
+	allowedSortArgs := []string{",", "first_name", "last_name", "created_at"}
 
 	for _, arg := range allowedSortArgs {
 		qp.Sort = strings.ReplaceAll(qp.Sort, arg, "")
 	}
 
 	if len(qp.Sort) > 0 {
-		return fmt.Errorf("sort argument `%v` does not fit list: %v", qp.Sort, allowedSortArgs)
+		return fmt.Errorf("sort argument `%v` does not fit list: %+v", qp.Sort, allowedSortArgs[1:])
 	}
 
 	return nil
