670d8d5593e55dcd5d986a83d2a91b19df7a9de8
Serhii Dzhulai <37537963+Brigant@users.noreply.github.com>
2022-11-03 10:12:15 +0200

    Fix: do not return dead people (#38)
    fix: do not return dead people

--- app/repository/pg/user.go
+++ app/repository/pg/user.go
@@ -58,7 +58,8 @@ func (r Repo) FindUser(id string) (*entities.User, error) {
 	const SQL = `
 			SELECT id, first_name, last_name, email, created_at 
 			FROM "user" 
-			WHERE id = $1;
+			WHERE id = $1
+			AND deleted_at is null;
 			`
 
 	if err := r.DB.QueryRow(SQL, id).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.CreatedAt); err != nil {
