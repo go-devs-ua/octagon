746037a92d400ef2e039d0d2f0cabbf675759f0c
Anton <pat280885@gmail.com>
2022-11-10 18:04:29 +0200

    fix: improved formatting and changed JSON compare

--- app/transport/rest/user_test.go
+++ app/transport/rest/user_test.go
@@ -26,7 +26,7 @@ func TestUserHandler_DeleteUser(t *testing.T) {
 		expStatusCode      int
 		expResponseBody    string
 	}{
-		"succes": {
+		"success": {
 			requestBody: `{"id": "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc"}`,
 			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
 				mock := NewMockUserUsecase(ctrl)
@@ -37,7 +37,8 @@ func TestUserHandler_DeleteUser(t *testing.T) {
 
 				return mock
 			},
-			expStatusCode: 204,
+			expResponseBody: "",
+			expStatusCode:   204,
 		},
 		"invalid_undefined_id": {
 			requestBody: `{"id": "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc"}`,
@@ -49,48 +50,32 @@ func TestUserHandler_DeleteUser(t *testing.T) {
 
 				return mock
 			},
-			expResponseBody: `
-									{
-										"message": "Not found",
-										"details": "no user found in DB"
-									}`,
-			expStatusCode: 404,
+			expResponseBody: `{"message":"Not found","details":"no user found in DB"}`,
+			expStatusCode:   404,
 		},
 		"invalid_bad_id_too_long": {
 			requestBody: `{"id": "1acef78b-55ef-43b1-8c2b-c9e36c9c11111"}`,
 			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
 				return nil
 			},
-			expResponseBody: `
-									{
-			  						"message": "Bad request",
-			 						"details": "invalid uuid: invalid UUID length: 37"
-									}`,
-			expStatusCode: 400,
+			expResponseBody: `{"message":"Bad request","details":"invalid uuid: invalid UUID length: 37"}`,
+			expStatusCode:   400,
 		},
 		"invalid_bad_id_too_short": {
 			requestBody: `{"id": "1acef78b-55ef-43b1-8c2b-c9e36c9c280"}`,
 			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
 				return nil
 			},
-			expResponseBody: `
-									{
-			  						"message": "Bad request",
-								    "details": "invalid uuid: invalid UUID length: 35"
-									}`,
-			expStatusCode: 400,
+			expResponseBody: `{"message":"Bad request","details":"invalid uuid: invalid UUID length: 35"}`,
+			expStatusCode:   400,
 		},
 		"invalid_bad_id_bad_symbpls": {
 			requestBody: `{"id": "%acef78b-55ef-43b1-8c2b-c9e36c9c2807"}`,
 			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
 				return nil
 			},
-			expResponseBody: `
-									{
-			  						"message": "Bad request",
-								    "details": "invalid uuid: invalid UUID format"
-									}`,
-			expStatusCode: 400,
+			expResponseBody: `{"message":"Bad request","details":"invalid uuid: invalid UUID format"}`,
+			expStatusCode:   400,
 		},
 		"invalid_internal_server_error": {
 			requestBody: `{"id": "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc"}`,
@@ -102,12 +87,8 @@ func TestUserHandler_DeleteUser(t *testing.T) {
 
 				return mock
 			},
-			expResponseBody: `
-									{
-										"message": "Internal server error",
-										"details": ""
-									}`,
-			expStatusCode: 500,
+			expResponseBody: `{"message":"Internal server error","details":""}`,
+			expStatusCode:   500,
 		},
 	}
 
@@ -123,7 +104,7 @@ func TestUserHandler_DeleteUser(t *testing.T) {
 			resp := httptest.NewRecorder()
 			uh.DeleteUser(resp, httptest.NewRequest(http.MethodDelete, "*", strings.NewReader(tt.requestBody)))
 			require.Equal(t, tt.expStatusCode, resp.Code)
-			if name != "succes" {
+			if len(tt.expResponseBody) > 0 {
 				require.JSONEq(t, tt.expResponseBody, resp.Body.String())
 			}
 		})
