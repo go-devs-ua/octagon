8a890efc7fd7ee4d40cc7a2d351b7b7419df61b3
Serhii <sergeyjuly@gmail.com>
2022-11-10 15:29:11 +0200

    fix: rename mock

--- app/transport/rest/contracts.go
+++ app/transport/rest/contracts.go
@@ -4,7 +4,7 @@ import (
 	"github.com/go-devs-ua/octagon/app/entities"
 )
 
-//go:generate mockgen -source=./contracts.go -destination=./mock_usecase.go -package=rest
+//go:generate mockgen -source=./contracts.go -destination=./mock_usecase_test.go -package=rest
 
 // UserUsecase represents User use-case layer.
 type UserUsecase interface {
