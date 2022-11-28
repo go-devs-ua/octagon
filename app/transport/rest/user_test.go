4f6754d5363a1a01d5054a16368379c891f686de
Anton-Hudz <98592774+Anton-Hudz@users.noreply.github.com>
2022-11-28 19:34:18 +0200

    feat: add test delete user (#43)
    * feat: add test for endpoint delete user
    
    * fix: improved formatting and changed JSON compare
    
    * fix: changed status code names
    
    * fix: changed test case name undefined -> notexisted
    
    * fix: changed: log level to const, resolved conflicts
    
    * fix: removed defer

--- app/transport/rest/user_test.go
+++ app/transport/rest/user_test.go
@@ -2,13 +2,15 @@ package rest
 
 import (
 	"errors"
-	"github.com/go-devs-ua/octagon/app/globals"
-	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
 	"path"
+	"strings"
 	"testing"
 
+	"github.com/go-devs-ua/octagon/app/globals"
+	"github.com/gorilla/mux"
+  
 	"github.com/go-devs-ua/octagon/app/entities"
 	"github.com/go-devs-ua/octagon/lgr"
 	"github.com/golang/mock/gomock"
@@ -291,6 +293,103 @@ func TestUserHandler_GetUserByID(t *testing.T) {
 
 			require.Equal(t, tt.expectedStatusCode, response.Code)
 			require.JSONEq(t, tt.expectedResponsetBody, response.Body.String())
+		})
+	}
+}
+
+func TestUserHandler_DeleteUser(t *testing.T) {
+	logger, err := lgr.New(lgr.InfoLevel)
+	if err != nil {
+		t.FailNow()
+	}
+
+	tests := map[string]struct {
+		requestBody        string
+		usecaseConstructor func(ctrl *gomock.Controller) UserUsecase
+		expStatusCode      int
+		expResponseBody    string
+	}{
+		"success": {
+			requestBody: `{"id": "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc"}`,
+			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
+				mock := NewMockUserUsecase(ctrl)
+
+				mock.EXPECT().Delete(entities.User{
+					ID: "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc",
+				}).Return(nil).Times(1)
+
+				return mock
+			},
+			expResponseBody: "",
+			expStatusCode:   http.StatusNoContent,
+		},
+		"invalid_notexisted_id": {
+			requestBody: `{"id": "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc"}`,
+			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
+				mock := NewMockUserUsecase(ctrl)
+				mock.EXPECT().Delete(entities.User{
+					ID: "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc",
+				}).Return(globals.ErrNotFound).Times(1)
+
+				return mock
+			},
+			expResponseBody: `{"message":"Not found","details":"no user found in DB"}`,
+			expStatusCode:   http.StatusNotFound,
+		},
+		"invalid_bad_id_too_long": {
+			requestBody: `{"id": "1acef78b-55ef-43b1-8c2b-c9e36c9c11111"}`,
+			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
+				return nil
+			},
+			expResponseBody: `{"message":"Bad request","details":"invalid uuid: invalid UUID length: 37"}`,
+			expStatusCode:   http.StatusBadRequest,
+		},
+		"invalid_bad_id_too_short": {
+			requestBody: `{"id": "1acef78b-55ef-43b1-8c2b-c9e36c9c280"}`,
+			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
+				return nil
+			},
+			expResponseBody: `{"message":"Bad request","details":"invalid uuid: invalid UUID length: 35"}`,
+			expStatusCode:   http.StatusBadRequest,
+		},
+		"invalid_bad_id_bad_symbpls": {
+			requestBody: `{"id": "%acef78b-55ef-43b1-8c2b-c9e36c9c2807"}`,
+			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
+				return nil
+			},
+			expResponseBody: `{"message":"Bad request","details":"invalid uuid: invalid UUID format"}`,
+			expStatusCode:   http.StatusBadRequest,
+		},
+		"invalid_internal_server_error": {
+			requestBody: `{"id": "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc"}`,
+			usecaseConstructor: func(ctrl *gomock.Controller) UserUsecase {
+				mock := NewMockUserUsecase(ctrl)
+				mock.EXPECT().Delete(entities.User{
+					ID: "dca5947d-3dfc-49f1-bc09-dd53ce7e71cc",
+				}).Return(errors.New("Internal error")).Times(1)
+
+				return mock
+			},
+			expResponseBody: `{"message":"Internal server error","details":""}`,
+			expStatusCode:   http.StatusInternalServerError,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			uh := UserHandler{
+				usecase: tt.usecaseConstructor(ctrl),
+				logger:  logger,
+			}
+			resp := httptest.NewRecorder()
+			uh.DeleteUser(resp, httptest.NewRequest(http.MethodDelete, "*", strings.NewReader(tt.requestBody)))
+			ctrl.Finish()
+			require.Equal(t, tt.expStatusCode, resp.Code)
+			if len(tt.expResponseBody) > 0 {
+				require.JSONEq(t, tt.expResponseBody, resp.Body.String())
+			}
 		})
 	}
 }
